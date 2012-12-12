856c2db0f094fb790dfddc203eea36f139663f2d
Mikael Brevik <mikaelb@mikaelb.net>
2012-12-12 16:06:08 +0100

    Added some tests

---
+++ test/terminal-notifier.js
@@ -0,0 +1,60 @@
+var notifier = require('../index')
+  , should = require('should')
+  , assert = require('assert');
+
+describe('node-notifier', function(){
+
+  describe('#notify()', function(){
+
+    it('should notify with a message', function(done){
+      
+      notifier.notify({
+        message: "Hello World"
+      }, function (err, response) {
+        response.type.should.equal("delivered");
+        done();
+      });
+
+    });
+
+    it('should be chainable', function(done){
+      
+      notifier.notify({
+        message: "First test"
+      }).notify({
+        message: "Second test"
+      }, function (err, response) {
+        response.type.should.equal("delivered");
+        response.response[0].should.equal("Notification delivered.");
+        done();
+      });
+
+    });
+
+    it('should be able to list all notifications', function(done){
+      notifier.notify({
+          list: "ALL"
+        }, function (err, response) {
+          response.response.length.should.equal(3);
+          done();
+        });
+    });
+
+
+    it('should be able to remove all messages', function(done){
+      notifier.notify({
+        remove: "ALL"
+      }, function (err, response) {
+        response.type.should.equal("removed");
+
+        notifier.notify({
+          list: "ALL"
+        }, function (err, response) {
+          assert(!response);
+          done();
+        });
+      });
+    });
+
+  });
+});
