54cbbe1534b88f510fb28e0b004b12df187f30a9
Mikael Brevik <mikaelbre@gmail.com>
2014-03-07 22:38:44 +0100

    Updates notifier to support growl and windows.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 var os = require('os');
-var notifier = (os.type() === 'Linux') ? 'notify-send' : 'terminal-notifier';
-module.exports = require('./lib/' + notifier);
+var send = require('./lib/notifiers/notify-send');
+var mac = require('./lib/notifiers/terminal-notifier');
+var growl =  require('./lib/notifiers/growl');
+
+switch(os.type()) {
+  case 'Linux':
+    module.exports = send;
+    break;
+  case 'Darwin':
+    module.exports = mac;
+    break;
+  default:
+    module.exports = growl;
+}
+
+module.exports.NotifySend = send;
+module.exports.NotificationCenter = mac;
+module.exports.Growl = growl;
--- test/terminal-notifier.js
+++ test/terminal-notifier.js
@@ -1,66 +1,77 @@
-var notifier = require('../')
+var NotificationCenter = require('../').NotificationCenter
   , should = require('should')
+  , os = require('os')
   , assert = require('assert');
 
-describe('node-notifier', function(){
+var notifier = new NotificationCenter();
 
-  describe('#notify()', function(){
+(function () {
 
-    before(function (done) {
-      notifier.notify({
-        remove: "ALL"
-      }, function () { done(); });
-    });
+  if (os.type() !== 'Darwin') {
+    console.log('Only tests for Mac for now.');
+    return;
+  }
 
-    it('should notify with a message', function(done){
+  describe('node-notifier', function(){
 
-      notifier.notify({
-        message: "Hello World"
-      }, function (err, response) {
-        response.type.should.equal("delivered");
-        done();
+    describe('#notify()', function(){
+
+      before(function (done) {
+        notifier.notify({
+          remove: "ALL"
+        }, function () { done(); });
       });
 
-    });
-
-    it('should be chainable', function(done){
-
-      notifier.notify({
-        message: "First test"
-      }).notify({
-        message: "Second test"
-      }, function (err, response) {
-        response.type.should.equal("delivered");
-        response.response[0].should.equal("Notification delivered.");
-        done();
-      });
-
-    });
-
-    it('should be able to list all notifications', function(done){
-      notifier.notify({
-          list: "ALL"
-        }, function (err, response) {
-          response.response.length.should.equal(3);
-          done();
-        });
-    });
-
-
-    it('should be able to remove all messages', function(done){
-      notifier.notify({
-        remove: "ALL"
-      }, function (err, response) {
-        response.type.should.equal("removed");
+      it('should notify with a message', function(done){
 
         notifier.notify({
-          list: "ALL"
+          message: "Hello World"
         }, function (err, response) {
-          assert(!response);
+          response.type.should.equal("delivered");
           done();
         });
-      });
-    });
 
+      });
+
+      it('should be chainable', function(done){
+
+        notifier.notify({
+          message: "First test"
+        }).notify({
+          message: "Second test"
+        }, function (err, response) {
+          response.type.should.equal("delivered");
+          response.response[0].should.equal("Notification delivered.");
+          done();
+        });
+
+      });
+
+      it('should be able to list all notifications', function(done){
+        notifier.notify({
+            list: "ALL"
+          }, function (err, response) {
+            response.response.length.should.equal(3);
+            done();
+          });
+      });
+
+
+      it('should be able to remove all messages', function(done){
+        notifier.notify({
+          remove: "ALL"
+        }, function (err, response) {
+          response.type.should.equal("removed");
+
+          notifier.notify({
+            list: "ALL"
+          }, function (err, response) {
+            assert(!response);
+            done();
+          });
+        });
+      });
+
+    });
   });
-});
+}());
