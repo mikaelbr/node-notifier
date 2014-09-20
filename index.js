8b8fce67153d23e4fc0bc40b6ec3c67077880a70
Mikael Brevik <mikaelbre@gmail.com>
2014-09-20 21:47:23 +0200

    Adds support for balloons in windows version pre 8

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var os = require('os');
 var send = require('./lib/notifiers/notify-send');
 var mac = require('./lib/notifiers/terminal-notifier');
-var win = require('./lib/notifiers/toaster');
+var win8 = require('./lib/notifiers/toaster');
 var growl =  require('./lib/notifiers/growl');
+var balloon =  require('./lib/notifiers/balloon');
+var utils =  require('./lib/utils');
 
 switch(os.type()) {
   case 'Linux':
@@ -12,7 +14,11 @@ switch(os.type()) {
     module.exports = mac;
     break;
   case 'Windows_NT':
-    module.exports = win;
+    if (utils.isLessThanWin8()) {
+      module.exports = balloon;
+    } else {
+      module.exports = win8;
+    }
     break;
   default:
     module.exports = growl;
@@ -20,5 +26,6 @@ switch(os.type()) {
 
 module.exports.NotifySend = send;
 module.exports.NotificationCenter = mac;
-module.exports.WindowsToaster = win;
+module.exports.WindowsToaster = win8;
+module.exports.WindowsBalloon = balloon;
 module.exports.Growl = growl;
