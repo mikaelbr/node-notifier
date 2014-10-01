3f6a2ccd521f653dd7654f16d6219e0236bb0f32
Mikael Brevik <mikaelbre@gmail.com>
2014-10-01 12:25:16 +0200

    Cleans up examples and code style

--- example/advanced.js
+++ example/advanced.js
@@ -2,15 +2,11 @@ var Notify = require('../');
 var notifier = new Notify();
 
 notifier.notify({
-  "title": "Phil Coulson",
-  "subtitle": "Agent of S.H.I.E.L.D.",
-  "message": "If I come out, will you shoot me? 'Cause then I won't come out.",
-  "sound": "Funk", // case sensitive
-  "appIcon": __dirname + "/coulson.jpg",
-  "contentImage": __dirname + "/coulson.jpg",
-  "open": "file://" + __dirname + "/coulson.jpg"
+  'title': 'Phil Coulson',
+  'subtitle': 'Agent of S.H.I.E.L.D.',
+  'message': 'If I come out, will you shoot me? \'Cause then I won\'t come out.',
+  'sound': 'Funk', // case sensitive
+  'appIcon': __dirname + '/coulson.jpg',
+  'contentImage': __dirname + '/coulson.jpg',
+  'open': 'file://' + __dirname + '/coulson.jpg'
 });
-
-setTimeout(function() {
-  console.log("Done");
-}, 5000);
--- example/message.js
+++ example/message.js
@@ -1,13 +1,15 @@
-var Notify = require("../index");
+var Notify = require('../index');
 var notifier = new Notify();
 
-notifier.notify({
-  "message": "Hello",
-  "wait": true
-}, function (err, data) {
-  if (err) {
-    return console.error("Error: ", err);
-  }
-
-  console.log("data:", data);
-});
+notifier
+  .notify({
+    'message': 'Hello',
+    'wait': true
+  }, function (err, data) {
+    // Will also wait until notification is closed.
+    console.log('Waited');
+    console.log(err, data);
+  })
+  .on('click', function () {
+    console.log(arguments);
+  });
