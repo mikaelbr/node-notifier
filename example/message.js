34aa3ca9e3ca8400a7f56d7556626eb23d675b20
Mikael Brevik <mikaelbre@gmail.com>
2014-10-01 09:01:41 +0200

    Updates terminal-notifier dependency to a special fork. Adds support for waiting

--- example/message.js
+++ example/message.js
@@ -2,17 +2,12 @@ var Notify = require("../index");
 var notifier = new Notify();
 
 notifier.notify({
-  "message": "Hello"
-}
-, function (err, data) {
+  "message": "Hello",
+  "wait": true
+}, function (err, data) {
   if (err) {
     return console.error("Error: ", err);
   }
 
-  console.log(data);
+  console.log("data:", data);
 });
-
-
-setTimeout(function() {
-  console.log("Done");
-}, 5000);
