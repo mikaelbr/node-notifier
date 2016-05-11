77844d8e00f4b4c45774fa3bc9b99b4af195ee72
刘祺 <qil@jumei.com>
2016-05-11 16:39:04 +0800

    https://github.com/mikaelbr/node-notifier/issues/99

---
+++ test/toaster.js
@@ -0,0 +1,44 @@
+var Notify = require('../notifiers/toaster')
+  , should = require('should')
+  , path = require('path')
+  , utils = require('../lib/utils')
+  , os = require('os')
+  , assert = require('assert');
+
+describe('WindowsToaster', function(){
+
+  before(function () {
+    this.original = utils.fileCommand;
+    this.originalType = os.type;
+    this.originalArch = os.arch;
+    this.originalRelease = os.release;
+    os.release = function () {
+      return "6.2.9200";
+    };
+    os.type = function () {
+      return "Windows_NT";
+    };
+  });
+
+  after(function () {
+    utils.fileCommand = this.original;
+    os.type = this.originalType;
+    os.arch = this.originalArch;
+    os.release = this.originalRelease;
+  });
+
+  it('should parse file protocol URL', function (done) {
+    utils.fileCommand = function (notifier, argsList, callback) {
+      argsList[1].should.eql(path.resolve("C:\\node-notifier\\test\\fixture\\coulson.jpg"));
+      done();
+    };
+
+    var notifier = new Notify();
+
+    notifier.notify({
+      'title': 'Heya',
+      'message': 'foo bar',
+      'icon': 'file:///C:/node-notifier/test/fixture/coulson.jpg'
+    });
+  });
+});
