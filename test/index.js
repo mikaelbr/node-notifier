25e3bdfbb0edf2a99e815f2d7f6ef3ce8a56d002
Foysal Ahamed <cfaion341@gmail.com>
2016-09-06 14:06:55 +0200

    Checks 2nd parameter of the notify() method to be a function

--- test/index.js
+++ test/index.js
@@ -8,6 +8,22 @@ describe('constructors', function(){
     should(notifier.notify).be.ok;
   });
 
+  it('should expect only a function callback as second parameter', function () {
+    var cb = function (err, data) {};
+    var data = { 'title': 'My notification' };
+
+    should(notifier.notify(data, cb)).be.ok;
+  });
+
+  it('should throw error when second parameter is not a function', function () {
+    var wrongParamOne = 200;
+    var wrongParamTwo = 'meaningless string';
+    var data = { 'title': 'My notification' };
+
+    notifier.notify.bind(notifier, data, wrongParamOne).should.throw(/^The second argument/);
+    notifier.notify.bind(notifier, data, wrongParamTwo).should.throw(/^The second argument/);
+  });
+
   it('should expose a default selected constructor function', function () {
     should(notifier instanceof notifier.Notification).be.ok;
   });
