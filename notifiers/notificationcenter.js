aa86d0d2f6cbe533eb175fe004d5e9bc761466c3
Jakob Krigovsky <jakob@krigovsky.com>
2018-06-10 19:31:55 +0200

    Prevent Spotlight from indexing terminal-notifier.app
    According to https://apple.stackexchange.com/a/92785, Spotlight does not index folders ending with `.noindex`.

--- notifiers/notificationcenter.js
+++ notifiers/notificationcenter.js
@@ -6,7 +6,7 @@ var Growl = require('./growl');
 var path = require('path');
 var notifier = path.join(
   __dirname,
-  '../vendor/terminal-notifier.app/Contents/MacOS/terminal-notifier'
+  '../vendor/mac.noindex/terminal-notifier.app/Contents/MacOS/terminal-notifier'
 );
 
 var EventEmitter = require('events').EventEmitter;
